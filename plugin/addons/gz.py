14426e96b130cbba0679edd257428a26f5950082
Pr2 <Pr2@github.com>
2019-08-11 11:46:34 +0200

    Make more screens adaptable for skinning. Without putting code twice in a skin.xml. Thanks to littlesat.

--- plugin/addons/gz.py
+++ plugin/addons/gz.py
@@ -20,7 +20,7 @@ class GunzipMenuScreen(ArchiverMenuScreen):
 
 	def __init__(self, session, sourcelist, targetlist):
 		super(GunzipMenuScreen, self).__init__(session, sourcelist, targetlist)
-
+		self.skinName = "ArchiverMenuScreen"
 		self.list.append((_("Unpack to current folder"), 1))
 		self.list.append((_("Unpack to %s") % self.targetDir, 2))
 		self.list.append((_("Unpack to %s") % config.usage.default_path.value, 3))
@@ -50,6 +50,7 @@ class UnpackInfoScreen(ArchiverInfoScreen):
 
 	def __init__(self, session, list, sourceDir, filename):
 		super(UnpackInfoScreen, self).__init__(session, list, sourceDir, filename)
+		self.skinName = "ArchiverInfoScreen"
 		self.pname = pname
 		self.pdesc = pdesc
 		self.pversion = pversion
