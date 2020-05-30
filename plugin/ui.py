820296becfa994535d529df6bb772d0e6b817e63
ims <ims21@users.sourceforge.net>
2020-05-30 23:05:13 +0200

    Fixed ngettext jobs, update .pot, .po

--- plugin/ui.py
+++ plugin/ui.py
@@ -502,7 +502,7 @@ class FileCommanderScreen(Screen, HelpableScreen, key_actions):
 			filtered = "(*)"
 
 		if self.jobs or self.jobs_old:
-			jobs = ngettext(_("(%d job)"), _("(%d jobs)"), (self.jobs+self.jobs_old)) % (self.jobs+self.jobs_old)
+			jobs = ngettext("(%d job)", "(%d jobs)", (self.jobs+self.jobs_old)) % (self.jobs+self.jobs_old)
 		else:
 			jobs = ""
 		self.setTitle(pname + " " + filtered + " " + jobs)
