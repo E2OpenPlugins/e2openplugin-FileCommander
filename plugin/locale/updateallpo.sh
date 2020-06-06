fd3441412d8ff994b9455b37e8b0b64218925d18
ims <ims21@users.sourceforge.net>
2020-06-06 13:53:10 +0200

    updateallpo.sh - added Q for generating .mo files

--- plugin/locale/updateallpo.sh
+++ plugin/locale/updateallpo.sh
@@ -74,19 +74,24 @@ printf "Merging pot files to create: $Plugin.pot\n"
 cat $Plugin-py.pot $Plugin-xml.pot | msguniq --no-wrap -o $Plugin.pot -
 OLDIFS=$IFS
 IFS=" "
+printf "\n"; read -n1 -r -p "Press 'M' to continue with generating .mo files" key; printf "\n"
 for lang in "${languages[@]}" ; do
 	if [ -f $lang$FilePath$Plugin.po ]; then 
 		printf "Updating existing translation file %s.po\n" $lang
 		msgmerge --backup=none --no-wrap -s -U $lang$FilePath$Plugin.po $Plugin.pot && touch $lang$FilePath$Plugin.po
 		msgattrib --no-wrap --no-obsolete $lang$FilePath$Plugin.po -o $lang$FilePath$Plugin.po
-		msgfmt -o $lang$FilePath$Plugin.mo $lang$FilePath$Plugin.po
+		if [ "$key" = 'M' ]; then
+			msgfmt -o $lang$FilePath$Plugin.mo $lang$FilePath$Plugin.po
+		fi
 	else
 		if [ ! -d $lang$FilePath ]; then
 			mkdir $lang$FilePath
 		fi
 		printf "New file created: %s, please add it to github before commit\n" $lang$FilePath$Plugin.po
 		msginit -l $lang$FilePath$Plugin.po -o $lang$FilePath$Plugin.po -i $Plugin.pot --no-translator
-		msgfmt -o $lang$FilePath$Plugin.mo $lang$FilePath$Plugin.po
+		if [ "$key" = 'M' ]; then
+			msgfmt -o $lang$FilePath$Plugin.mo $lang$FilePath$Plugin.po
+		fi
 	fi
 done
 rm $Plugin-py.pot $Plugin-xml.pot
