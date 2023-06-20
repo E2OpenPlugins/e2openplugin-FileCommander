76d5946ce29f2c6ba4bd8a80c5df1f27e3b52b56
Pr2 <pr2git@users.noreply.github.com>
2023-06-20 17:09:03 +0200

    All po update and pot

--- plugin/locale/updateallpo.sh
+++ plugin/locale/updateallpo.sh
@@ -69,7 +69,7 @@ printf "Creating temporary file $Plugin-py.pot\n"
 find $findoptions .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d $Plugin -s -o $Plugin-py.pot {} \+
 $localgsed --in-place $Plugin-py.pot --expression=s/CHARSET/UTF-8/
 printf "Creating temporary file $Plugin-xml.pot\n"
-find $findoptions .. -name "*.xml" -exec python xml2po.py {} \+ > $Plugin-xml.pot
+find $findoptions .. -name "*.xml" -exec python3 xml2po.py {} \+ > $Plugin-xml.pot
 printf "Merging pot files to create: $Plugin.pot\n"
 cat $Plugin-py.pot $Plugin-xml.pot | msguniq --no-wrap -o $Plugin.pot -
 OLDIFS=$IFS
